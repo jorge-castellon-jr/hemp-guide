2752206c0f070cd6a49bc3145c066894f2f7ee98
Jorge Castellon Jr <jorge.castellon.jr@gmail.com>
2026-02-25 11:24:16 -0600

    Updates images to use cf hosted images

--- src/content/sections.ts
+++ src/content/sections.ts
@@ -1,26 +1,33 @@
-import type { SocialLink } from '../components/SocialIcons';
+import type { SocialLink } from "../components/SocialIcons";
 
 export const socialLinks: SocialLink[] = [
-  { type: 'twitter', href: 'https://twitter.com/MEHarmon', label: 'X' },
-  { type: 'instagram', href: 'https://www.instagram.com/meharmon/', label: 'Instagram' },
-  { type: 'linkedin', href: 'https://www.linkedin.com/in/mattheweharmon/', label: 'LinkedIn' },
-  { type: 'email', href: 'mailto:info@farmbridge.com', label: 'Email' },
+  { type: "twitter", href: "https://twitter.com/MEHarmon", label: "X" },
+  {
+    type: "instagram",
+    href: "https://www.instagram.com/meharmon/",
+    label: "Instagram",
+  },
+  {
+    type: "linkedin",
+    href: "https://www.linkedin.com/in/mattheweharmon/",
+    label: "LinkedIn",
+  },
+  { type: "email", href: "mailto:info@farmbridge.com", label: "Email" },
 ];
 
 /** Images downloaded from reference index.html (hempguide.com) */
 export const images = {
-  book: '/images/Matthew-Harmon-Making-a-Billion-Dollars-from-Hemp-1W.png',
-  hemp: '/images/447-hempseed-photo-credit-matthew-harmon-lomzf-e1614263882850.jpg',
-  documentary: 'https://placehold.co/800x600/555/eee?text=Documentary',
-  matthew: '/images/447-matthew-harmon-headshot.jpeg',
-  redLine: '/images/red-lineW-copy.png',
-  favicon: '/images/Hemp-Guide-favicon.png',
-  farmbridgeLogo: '/images/Farmbridge-Logotype-01.png',
+  book: "https://imagedelivery.net/1yljJPusD0juUBvTw4tCwg/989ba760-fe9d-4158-f9a6-6d9e2f8b7300/public",
+  hemp: "/images/447-hempseed-photo-credit-matthew-harmon-lomzf-e1614263882850.jpg",
+  matthew:
+    "https://imagedelivery.net/1yljJPusD0juUBvTw4tCwg/d87549ab-a931-4034-8063-833499e28900/public",
+  farmbridgeLogo: "/images/Farmbridge-Logotype-01.png",
 } as const;
 
 /** Video downloaded from reference index.html (hempguide.com) */
 export const videos = {
-  header: '/videos/website_header_video_20_sec-1.mp4',
+  header:
+    "https://pub-7b9b3656486f4ea08414d1a6806c9532.r2.dev/website_header_video_20_sec-1.mp4",
 } as const;
 
 /** @deprecated Use images instead */
